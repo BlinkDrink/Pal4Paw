c9fbf4f67320314cc831a2932493452e190e72d2
Petko Petkov <evrolan1404@gmail.com>
2020-04-06 11:08:32 +0300

    Add Notification.cs. Add Notifications Table.

---
+++ Data/DogCarePlatform.Data.Models/Notification.cs
@@ -0,0 +1,23 @@
+﻿using DogCarePlatform.Data.Common.Models;
+using System;
+
+namespace DogCarePlatform.Data.Models
+{
+    public class Notification : BaseDeletableModel<string>
+    {
+        public Notification()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public DateTime ReceivedOn { get; set; }
+
+        public string OwnerId { get; set; }
+
+        public Owner Owner { get; set; }
+
+        public string DogsitterId { get; set; }
+
+        public Dogsitter Dogsitter { get; set; }
+    }
+}
---
+++ Data/DogCarePlatform.Data/Migrations/20200406075448_AddNotificationTable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DogCarePlatform.Data.Migrations
+{
+    public partial class AddNotificationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                    ReceivedOn = table.Column<DateTime>(nullable: false),
+                    OwnerId = table.Column<string>(nullable: true),
+                    DogsitterId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notifications_Dogsitters_DogsitterId",
+                        column: x => x.DogsitterId,
+                        principalTable: "Dogsitters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Notifications_Owners_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Owners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_DogsitterId",
+                table: "Notifications",
+                column: "DogsitterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_IsDeleted",
+                table: "Notifications",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_OwnerId",
+                table: "Notifications",
+                column: "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifications");
+        }
+    }
+}
