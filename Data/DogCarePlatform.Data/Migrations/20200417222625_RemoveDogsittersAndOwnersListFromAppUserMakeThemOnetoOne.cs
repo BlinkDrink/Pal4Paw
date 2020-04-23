c16628fa35c1ce6e78850e7028aed787aa99f4df
Petko Petkov <evrolan1404@gmail.com>
2020-04-23 21:12:46 +0300

    Commit the migrations.

---
+++ Data/DogCarePlatform.Data/Migrations/20200417222625_RemoveDogsittersAndOwnersListFromAppUserMakeThemOnetoOne.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DogCarePlatform.Data.Migrations
+{
+    public partial class RemoveDogsittersAndOwnersListFromAppUserMakeThemOnetoOne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Owners_UserId",
+                table: "Owners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dogsitters_UserId",
+                table: "Dogsitters");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owners_UserId",
+                table: "Owners",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dogsitters_UserId",
+                table: "Dogsitters",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Owners_UserId",
+                table: "Owners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dogsitters_UserId",
+                table: "Dogsitters");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owners_UserId",
+                table: "Owners",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dogsitters_UserId",
+                table: "Dogsitters",
+                column: "UserId");
+        }
+    }
+}
